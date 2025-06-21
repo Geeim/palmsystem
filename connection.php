cb8285eccf1aac93799cfbf063582541caa5e787
Geeim <mr.jamesmateo22@gmail.com>
2025-06-21 14:31:22 +0800

    try

--- connection.php
+++ connection.php
@@ -1,18 +1,15 @@
 <?php
-	$Hostname = getenv("MYSQLHOST");
-$Username = getenv("MYSQLUSER");
-$Password = getenv("MYSQLPASSWORD");
-$DBname   = getenv("MYSQLDATABASE");
-$Port     = getenv("MYSQLPORT");
 
+echo $Hostname = "sql103.infinityfree.com"; // HOST
+$Username = "if0_39284576";            // USERNAME
+$Password = "t1Nj4FsjrU";              // PASSWORD
+$DBname   = "if0_39284576_palm";       // DB NAME
+$Port     = 3306;                      // PORT
 
+$con = mysqli_connect($Hostname, $Username, $Password, $DBname, $Port);    
 
-echo "Host: " . getenv("DB_HOST") . "<br>";
-echo "User: " . getenv("DB_USER") . "<br>";
-echo "DB: " . getenv("DB_NAME") . "<br>";
-
-echo "Host: " . getenv("MYSQLHOST") . "<br>";
-echo "User: " . getenv("MYSQLUSER") . "<br>";
-echo "DB: " . getenv("MYSQLDATABASE") . "<br>";
-
+	if (!$con){
+		echo "Connection failed!";
+		exit();
+	}
 ?>
