b6a2c67ecb348f755a17939c32247b8c4e3ed02c
Geeim <mr.jamesmateo22@gmail.com>
2025-06-21 14:15:11 +0800

    Try Again

--- connection.php
+++ connection.php
@@ -1,15 +1,15 @@
 <?php
+	$Hostname = getenv("DB_HOST");
+	$Username = getenv("DB_USER");
+	$Password = getenv("DB_PASS");
+	$DBname = getenv("DB_NAME");
+	$Port = getenv("DB_PORT");
 
-$Hostname = "sql103.infinityfree.com"; // HOST
-$Username = "if0_39284576";            // USERNAME
-$Password = "t1Nj4FsjrU";              // PASSWORD
-$DBname   = "if0_39284576_palm";       // DB NAME
-$Port     = 3306;                      // PORT
+	$con = mysqli_connect($Hostname, $Username, $Password, $DBname, $Port);
 
-$con = mysqli_connect($Hostname, $Username, $Password, $DBname, $Port);    
+if (!$con){
+    echo "Connection failed!";
+    exit();
+}
 
-	if (!$con){
-		echo "Connection failed!";
-		exit();
-	}
 ?>
